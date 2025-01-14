3742d47ad2e16456b485fa1a09b9eff4e3f48126
DEVANGBAJPAI23 <devangbajpai69@gmail.com>
2025-01-14 14:07:29 +0530

    updates

--- src/components/Freebook.jsx
+++ src/components/Freebook.jsx
@@ -10,11 +10,12 @@ import Cards from "./Cards";
 
 function Freebook() {
   const [book, setBook] = useState([]);
+  // const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getBook = async () => {
       try {
         const res = await axios.get("http://localhost:4001/book");
-
+        console.log(res);
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
         setBook(data);
@@ -24,63 +25,75 @@ function Freebook() {
     };
     getBook();
   }, []);
-    
-    var settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-    };
+
+  // if (book.length !== 0) {
+  //   setLoading(false);
+  // }
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
-    
     <>
-    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-800 dark:text-white'>
+      {/* {loading && (
         <div>
-        <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
-        <p>
-        Dive into a vast collection of free books and embark on endless literary adventures. Whether you're into fiction, non-fiction, or academic texts, Bookshala has something for everyone.
-        </p>
+          <img src="../../public/loader.gif"></img>
         </div>
+      )} */}
+      {/* {!loading && ( */}
+        <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-800 dark:text-white">
+          <div>
+            <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+            <p>
+              Dive into a vast collection of free books and embark on endless
+              literary adventures. Whether you're into fiction, non-fiction, or
+              academic texts, Bookshala has something for everyone.
+            </p>
+          </div>
 
-    <div>
-    <Slider {...settings}>
-        {list.map((item)=>(
-            <Cards item={item} key={item.id}/>
-        ) )}
-      </Slider>  
-    </div>
-    </div>
+          <div>
+            <Slider {...settings}>
+              {list.map((item) => (
+                <Cards item={item} key={item.id} />
+              ))}
+            </Slider>
+          </div>
+        </div>
+      {/* )} */}
     </>
-  )
+  );
 }
 
 export default Freebook;
